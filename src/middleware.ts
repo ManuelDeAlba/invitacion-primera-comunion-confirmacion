a3c87cb02e78d66b296a08c1b82575637090f0a0
Manuel De Alba <ferman5903@gmail.com>
2025-06-13 19:26:54 -0600

    Fix server side rendering in the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,37 @@
 import { defineMiddleware } from "astro:middleware";
 import { verificarToken } from "@/lib/auth-token";
 
-export const onRequest = defineMiddleware(async (context, next) => {
-    const url = new URL(context.request.url);
-    
-    // Variables to store the authentication status and role
-    let valido, rol;
+import type { APIContext } from "astro";
 
+function validateAuth(context: APIContext){
     // Get the auth token from cookies
     const token = context.cookies.get("auth-token");
-    // If the token exists, verify in the database
-    if(token) ({ auth: valido, rol } = verificarToken(token.value));
+
+    if(!token) return {
+        token: null,
+        valido: false,
+        rol: null,
+    }
+
+    // If the token exists, verify it with jwt and get the role
+    let { auth: valido, rol } = verificarToken(token.value);
     // Store the role in locals for later use
     if(valido && rol) context.locals.rol = rol;
 
+    return {
+        valido,
+        rol,
+        token,
+    }
+}
+
+export const onRequest = defineMiddleware(async (context, next) => {
+    const url = new URL(context.request.url);
+
     // If the user is already authenticated, redirect to the dashboard (/respuestas)
     if(url.pathname == "/login"){
+        const { token, valido } = validateAuth(context);
+
         if (token && valido) {
             return context.redirect("/respuestas");
         }
@@ -24,6 +40,8 @@ export const onRequest = defineMiddleware(async (context, next) => {
     }
 
     if (url.pathname.startsWith("/respuestas")) {
+        const { token, valido } = validateAuth(context);
+
         // If the token does not exist or the token is invalid, redirect to the login page
         if (!token || !valido) {
             return context.redirect("/login");
